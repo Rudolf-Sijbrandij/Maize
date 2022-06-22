651030f5fce6c9beef0ff6190fa41f3cbe9ff937
Rudolf <rudolf.sijbrandij@student.hu.nl>
2022-06-22 14:24:00 +0200

    Finished Tremaux maze solving algorithm

--- algoritmes.py
+++ algoritmes.py
@@ -42,3 +42,39 @@ def dfs(rows, columns):
             maze[current[0]][current[1]] = 1
             maze[wall[0]][wall[1]] = 1
     return maze
+
+
+def tremaux(maze):
+    def unvisited_neighbor(cur, vis, mark):
+        curx = cur[0]
+        cury = cur[1]
+        neighbors = [[curx - 1, cury], [curx, cury + 1], [curx + 1, cury], [curx, cury - 1]]
+        shuffle(neighbors)
+        for neighbor in neighbors:
+            check = True
+            if neighbor[0] < 0 or neighbor[1] < 0:
+                check = False
+            elif neighbor[0] > rows - 1 or neighbor[1] > columns - 1:
+                check = False
+            if neighbor not in mark and neighbor not in vis and check and maze[neighbor[0]][neighbor[1]] == 1:
+                return neighbor
+        return [-1, -1]
+
+    current = [0, 0]
+    solution = [current]
+    visited = []
+    mark2 = []
+    rows = len(maze)
+    columns = len(maze[0])
+    while current != [rows - 1, columns - 1]:
+        current = solution[len(solution) - 1]
+        solution.pop()
+        while current != [-1, -1]:
+            if current == [rows - 1, columns - 1]:
+                return solution
+            if current in visited:
+                mark2.append(current)
+            visited.append(current)
+            current = unvisited_neighbor(current, visited, mark2)
+            solution.append(current)
+    return solution
--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 from tkinter import *
 from tkinter import ttk
-from algoritme import dfs
+from algoritmes import *
 
 
 class Window:
     def __init__(self, master):
         self.master = master
         self.master.title("Maize")
-        self.master.geometry("620x620")
+        self.master.geometry("620x680")
         self.master.configure(bg='#1b2838')
         self.master.iconbitmap('images/Maize.ico')
-        self.max_size = 71
+        self.quick = 99  # doe als -1 om het invul proces NIET over te slaan
+        self.max_size = 99
         self.min_size = 21
         self.label_text = "Vul 2 oneven getallen in, minimaal "\
                           + str(self.min_size) + " en maximaal " + str(self.max_size)
         self.rows = -1
         self.columns = -1
-        self.cell_size = 8  # pixels
+        self.cell_size = 6  # pixels
+        self.draw_count = 0
         self.initial()
 
     def initial(self):
-        for i in self.master.winfo_children():
-            i.destroy()
+        if self.quick != -1:
+            self.rows = self.quick
+            self.columns = self.quick
+            self.initiate()
+            return
         frame1 = Frame(self.master, width=200, height=200, bg='#1b2838')
         frame1.place(relx=0.5, rely=0.5, anchor=CENTER)
         label = Label(frame1, text=self.label_text, bg='#1b2838', fg='#FFFFFF', font=("Arial", 15))
         label.grid(row=0, column=0, pady=2)
         self.entry1 = Entry(frame1)
+        self.entry1.insert(0, "Hoogte")
         self.entry1.grid(row=1, column=0, pady=2)
         self.entry2 = Entry(frame1)
+        self.entry2.insert(0, "Breedte")
         self.entry2.grid(row=2, column=0, pady=2)
         button1 = ttk.Button(frame1, text="Confirm", command=self.check)
         button1.grid(row=3, column=0, pady=2)
+        return
 
     def check(self):
         try:
@@ -54,18 +62,31 @@ class Window:
             self.initial()
             return
         else:
-            self.register()
+            self.initiate()
             return
 
-    def register(self):
-        def draw(draw_row, draw_col, draw_color):
-            x1 = (draw_col + 0.75) * self.cell_size
-            y1 = (draw_row + 0.75) * self.cell_size
-            x2 = x1 + self.cell_size
-            y2 = y1 + self.cell_size
-            frame2.create_rectangle(x1, y1, x2, y2, fill=draw_color)
-            return
+    def draw(self, draw_row, draw_col, draw_color, frame):
+        x1 = (draw_col + 0.75) * self.cell_size
+        y1 = (draw_row + 0.75) * self.cell_size
+        x2 = x1 + self.cell_size
+        y2 = y1 + self.cell_size
+        frame.create_rectangle(x1, y1, x2, y2, fill=draw_color, outline=draw_color)
+        return
 
+    def draw_maze(self, maze, frame):
+        for row in range(len(maze)):
+            for col in range(len(maze[row])):
+                if maze[row][col] == 1:
+                    color = 'white'
+                else:
+                    color = 'black'
+                self.draw(row, col, color, frame)
+                if self.draw_count == 0:
+                    frame.update()
+        self.draw_count += 1
+        return
+
+    def initiate(self):
         for i in self.master.winfo_children():
             i.destroy()
         width = (self.columns + 1)*self.cell_size
@@ -73,16 +94,24 @@ class Window:
         frame2 = Canvas(self.master, width=width, height=height, bg='#000000')
         frame2.place(relx=0.5, rely=0.5, anchor=CENTER)
         maze = dfs(self.rows, self.columns)
-        for row in range(len(maze)):
-            for col in range(len(maze[row])):
-                if maze[row][col] == 1:
-                    color = 'white'
-                else:
-                    color = 'black'
-                draw(row, col, color)
-                frame2.update()
-        draw(0, 0, 'green')
-        draw(self.rows - 1, self.columns - 1, 'red')
+        self.draw_maze(maze, frame2)
+        self.draw(0, -1, 'green', frame2)
+        self.draw(self.rows - 1, self.columns, 'red', frame2)
+        frame3 = Canvas(self.master, width=25, height=10)
+        frame3.pack(side=TOP)
+        button1 = ttk.Button(frame3, text="Solve", command=lambda: self.solve(maze, frame2))
+        button1.grid(row=3, column=0, pady=2)
+        return
+
+    def solve(self, maze, frame):
+        solution = tremaux(maze)
+        self.draw_maze(maze, frame)
+        self.draw(0, 0, 'blue', frame)
+        for coord in solution:
+            print(coord)
+            self.draw(coord[0], coord[1], 'blue', frame)
+            frame.update()
+        return
 
 
 root = Tk()
