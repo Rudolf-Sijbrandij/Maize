7fe8c4f48bd43216bb6ccec5231f3a00bdeed089
Rudolf <rudolf.sijbrandij@student.hu.nl>
2022-06-23 13:31:37 +0200

    Fixed bugs, added disjointed maze option and added commentary and docstrings to code

--- algoritmes.py
+++ algoritmes.py
@@ -1,22 +1,69 @@
 from random import *
 
 
-def dfs(rows, columns):
-    def unvisited_neighbor(cur, vis):
-        curx = cur[0]
-        cury = cur[1]
-        neighbors = [[curx - 2, cury], [curx, cury + 2], [curx + 2, cury], [curx, cury - 2]]
-        shuffle(neighbors)
-        for neighbor in neighbors:
-            check = True
-            if neighbor[0] < 0 or neighbor[1] < 0:
-                check = False
-            elif neighbor[0] > rows - 1 or neighbor[1] > columns - 1:
-                check = False
-            if neighbor not in vis and check:
-                return neighbor
-        return [-1, -1]
+def unvisited_neighbour(rows, columns, cur, vis, maze_type, dif, maze):
+    """
+    Kijkt in alle 4 richtingen; omhoog, omlaag, links en rechts, of er een stap die richting in gemaakt kan worden
+    zo niet, returned de functie [[-1, -1], [-1, -1]] en weet het programma dat hij terug moet totdat hij weer ergens
+    anders wel verder kan. Als hij wel verder kan, dan returned hij de coördinaten van de nieuwe cell.
 
+    Args:
+        rows (int):     De hoeveelheid rows die de gebruiker heeft opgegeven voor de grootte van de maze.
+        columns (int):  De hoeveelheid columns die de gebruiker heeft opgegeven voor de grootte van de maze.
+        cur (list):     De coördinaten van de huidige cell waarvoor je naar neighbours kijkt.
+        vis (list):     Een lijst van coördinaten van al eerder bezochte cellen.
+        maze_type (int):De maze type, als het 0 is is het een normale maze, als het 1 is is het een disjointed maze.
+                            Alleen relevant als het om het generaten van een maze gaat
+        dif (int):      Een getal, 0 of 1, dat aangeeft of de functie wordt gebruikt door de dfs() functie of door
+                            de Tremaux() functie, gaat erom of er stappen van 1 of van 2 gemaakt worden.
+        maze (list):    Alleen relevant voor Tremaux, laat de functie kijken of de stap die hij maakt wel op het pad is
+                            en niet door een muur heen gaat
+
+    Returns:
+        list:   Een lijst met coordinaten van een neighbour 1 stap verder en 2 stappen verder
+                    of als er geen geldige neighbours zijn een lijst met 2x [-1, -1] die aangeeft dat het programma
+                    weer terug moet omdat het doodloopt of niet verder kan
+    """
+    curx = cur[0]
+    cury = cur[1]
+    up = [[curx, cury - 2], [curx, cury - 1]]
+    down = [[curx, cury + 2], [curx, cury + 1]]
+    right = [[curx + 2, cury], [curx + 1, cury]]
+    left = [[curx - 2, cury], [curx - 1, cury]]
+    neighbors = [down, up, right, left]
+    shuffle(neighbors)
+    for neighbor in neighbors:
+        check = True
+        if neighbor[dif][0] < 0 or neighbor[dif][1] < 0:
+            check = False
+        elif neighbor[dif][0] > rows - 1 or neighbor[dif][1] > columns - 1:
+            check = False
+        if maze_type == 1:
+            rand = randrange(0, int(((rows + columns / 2) / 1.5)))
+            if (neighbor[dif] not in vis or rand == 1) and check:
+                return neighbor
+        elif dif == 1:
+            if neighbor[dif] not in vis and check and maze[neighbor[dif][0]][neighbor[dif][1]] == 1:
+                return neighbor
+        else:
+            if neighbor[dif] not in vis and check:
+                return neighbor
+    return [[-1, -1], [-1, -1]]
+
+
+def dfs(rows, columns, maze_type):
+    """
+    Doormiddel van de dimensies van de gebruiker maakt het algoritme een doolhof doormiddel van het
+    Depth First Search maze generation algoritme.
+
+    Args:
+        rows (int): De hoeveelheid rows die de gebruiker heeft opgegeven voor de grootte van de maze
+        columns (int): De hoeveelheid columns die de gebruiker heeft opgegeven voor de grootte van de maze
+        maze_type (int): De maze type, als het 0 is is het een normale maze, als het 1 is is het een disjointed maze
+
+    Returns:
+        list: een maze waar muren 0 zijn en paden 1
+    """
     maze = []
     stack = []
     visited = []
@@ -31,13 +78,11 @@ def dfs(rows, columns):
         maze[current[0]][current[1]] = 1
         stack.pop()
         while current != [-1, -1]:
-            nextcell = unvisited_neighbor(current, visited)
+            neighbor_list = unvisited_neighbour(rows, columns, current, visited,maze_type, 0, maze)
+            nextcell = neighbor_list[0]
+            wall = neighbor_list[1]
             stack.append(nextcell)
             visited.append(current)
-            if (current[0] - nextcell[0]) != 0 or (nextcell[0] - current[0]) != 0:
-                wall = [int((nextcell[0] + current[0]) / 2), nextcell[1]]
-            else:
-                wall = [nextcell[0], int((nextcell[1] + current[1]) / 2)]
             current = nextcell
             maze[current[0]][current[1]] = 1
             maze[wall[0]][wall[1]] = 1
@@ -45,25 +90,19 @@ def dfs(rows, columns):
 
 
 def tremaux(maze):
-    def unvisited_neighbor(cur, vis, mark):
-        curx = cur[0]
-        cury = cur[1]
-        neighbors = [[curx - 1, cury], [curx, cury + 1], [curx + 1, cury], [curx, cury - 1]]
-        shuffle(neighbors)
-        for neighbor in neighbors:
-            check = True
-            if neighbor[0] < 0 or neighbor[1] < 0:
-                check = False
-            elif neighbor[0] > rows - 1 or neighbor[1] > columns - 1:
-                check = False
-            if neighbor not in mark and neighbor not in vis and check and maze[neighbor[0]][neighbor[1]] == 1:
-                return neighbor
-        return [-1, -1]
+    """
+    Zoekt een pad naar het einde van de maze vanaf het begin doormiddel van het Trémaux's maze solving algorithm.
+    Maakt gebruik van een eerder gemaakte maze van het maze generation algorithm.
 
+    Args:
+        maze (list): Een lijst met de opbouw [rows[columns]] waar 0 een muur is en 1 een pad.
+
+    Returns:
+        list: een lijst van coördinaten van het pad naar het einde van de maze.
+    """
     current = [0, 0]
     solution = [current]
     visited = []
-    mark2 = []
     rows = len(maze)
     columns = len(maze[0])
     while current != [rows - 1, columns - 1]:
@@ -72,9 +111,10 @@ def tremaux(maze):
         while current != [-1, -1]:
             if current == [rows - 1, columns - 1]:
                 return solution
-            if current in visited:
-                mark2.append(current)
             visited.append(current)
-            current = unvisited_neighbor(current, visited, mark2)
+            nextcell = unvisited_neighbour(rows, columns, current, visited, 0, 1, maze)[1]
+            if current not in solution and nextcell != [-1, -1]:
+                solution.append(current)
+            current = nextcell
             solution.append(current)
     return solution
--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 from tkinter import *
 from tkinter import ttk
-from algoritmes import *
+from algoritmes import dfs, tremaux
 
 
 class Window:
     def __init__(self, master):
+
+        # maakt de hoofd window
         self.master = master
         self.master.title("Maize")
         self.master.geometry("620x680")
         self.master.configure(bg='#1b2838')
         self.master.iconbitmap('images/Maize.ico')
-        self.quick = 99  # doe als -1 om het invul proces NIET over te slaan
+
+        # een aantal developer settings
+        self.quick = -1  # doe als -1 om het invul proces NIET over te slaan, anders een getal tussen de max_size en
+        # min_size om direct de dimensies van de maze in te vullen
+
         self.max_size = 99
-        self.min_size = 21
-        self.label_text = "Vul 2 oneven getallen in, minimaal "\
+        self.min_size = 33
+        self.cell_size = 6  # pixel grootte van de maze cellen
+        self.label_text = "Vul 2 oneven getallen in, minimaal " \
                           + str(self.min_size) + " en maximaal " + str(self.max_size)
+
+        # een paar variabelen alvast aanmaken
         self.rows = -1
         self.columns = -1
-        self.cell_size = 6  # pixels
         self.draw_count = 0
+
+        # het openen van het begin window
         self.initial()
 
     def initial(self):
+        # als de self.quick niet -1 is dan slaat hij deze window over zodat je niet constant de hoogte en breedte in
+        # hoeft te vullen
         if self.quick != -1:
             self.rows = self.quick
             self.columns = self.quick
-            self.initiate()
+            self.initiate(0)  # 0 voor normaal, 1 voor disjointed
             return
+
+        # maakt de frame bovenop de master window
         frame1 = Frame(self.master, width=200, height=200, bg='#1b2838')
         frame1.place(relx=0.5, rely=0.5, anchor=CENTER)
         label = Label(frame1, text=self.label_text, bg='#1b2838', fg='#FFFFFF', font=("Arial", 15))
@@ -37,35 +51,56 @@ class Window:
         self.entry2 = Entry(frame1)
         self.entry2.insert(0, "Breedte")
         self.entry2.grid(row=2, column=0, pady=2)
-        button1 = ttk.Button(frame1, text="Confirm", command=self.check)
+        button1 = ttk.Button(frame1, text="Normal", command=lambda: self.check(0))
         button1.grid(row=3, column=0, pady=2)
+        button2 = ttk.Button(frame1, text="Disjointed", command=lambda: self.check(1))
+        button2.grid(row=4, column=0, pady=2)
         return
 
-    def check(self):
+    def check(self, maze_type):
         try:
+            # probeerd de waardes uit entry1 en entry2, als er niks in is gevult gaat hij naar de except
             self.rows = int(self.entry1.get())
             self.columns = int(self.entry2.get())
         except ValueError:
             self.label_text = "Vul een geldig getal in"
             self.initial()
             return
+
+        # checked of de ingevulde getallen te groot zijn
         if self.rows > self.max_size or self.columns > self.max_size:
             self.label_text = "Het getal mag niet groter zijn dan" + str(self.max_size)
             self.initial()
             return
+        # checked of de ingevulde getallen te klein zijn
         elif self.rows < self.min_size or self.columns < self.min_size:
             self.label_text = "Het getal mag niet kleiner zijn dan" + str(self.min_size)
             self.initial()
             return
+
+        # checked of de ingvulde getallen wel oneven zijn, als ze niet oneven zijn werkt de maze niet goed
         elif self.rows % 2 == 0 or self.columns % 2 == 0:
             self.label_text = "Vul een oneven getal in"
             self.initial()
             return
         else:
-            self.initiate()
+            # als alle tests doorstaan zijn gaat hij verder naar de initiate functie
+            self.initiate(maze_type)
             return
 
     def draw(self, draw_row, draw_col, draw_color, frame):
+        """
+        Tekend een vierkant op de goede plaats op het opgegeven frame met de opgegeven kleur.
+
+        Args:
+            draw_row (int): De row waarop het vierkant moet komen.
+            draw_col (int): De column waarop het vierkant moet komen.
+            draw_color (string): De kleur van het vierkant.
+            frame (frame): De tkinter frame waarop het vierkant komt
+
+        Returns:
+            None
+        """
         x1 = (draw_col + 0.75) * self.cell_size
         y1 = (draw_row + 0.75) * self.cell_size
         x2 = x1 + self.cell_size
@@ -74,6 +109,7 @@ class Window:
         return
 
     def draw_maze(self, maze, frame):
+        # gaat elke cell af om er een vierkant te tekenen om de maze te maken
         for row in range(len(maze)):
             for col in range(len(maze[row])):
                 if maze[row][col] == 1:
@@ -83,20 +119,33 @@ class Window:
                 self.draw(row, col, color, frame)
                 if self.draw_count == 0:
                     frame.update()
+
+        # houd bij hoe vaak de maze al gemaakt is om niet vaker dan 1x de animatie te spelen
         self.draw_count += 1
         return
 
-    def initiate(self):
+    def initiate(self, maze_type):
+        # verwijderd de eerdere frame met het invullen van de dimensies
         for i in self.master.winfo_children():
             i.destroy()
-        width = (self.columns + 1)*self.cell_size
-        height = (self.rows + 1)*self.cell_size
+
+        # de pixel grootte van de maze berekenen
+        width = (self.columns + 1) * self.cell_size
+        height = (self.rows + 1) * self.cell_size
+
+        # de maze frame maken
         frame2 = Canvas(self.master, width=width, height=height, bg='#000000')
         frame2.place(relx=0.5, rely=0.5, anchor=CENTER)
-        maze = dfs(self.rows, self.columns)
+
+        # de maze maken door de functie uit het andere python bestand te callen
+        maze = dfs(self.rows, self.columns, maze_type)
+
+        # het visueel maken van de maze met nog een begin en een eind in het groen en rood
         self.draw_maze(maze, frame2)
         self.draw(0, -1, 'green', frame2)
         self.draw(self.rows - 1, self.columns, 'red', frame2)
+
+        # een extra frame maken voor de knop om de maze te solven
         frame3 = Canvas(self.master, width=25, height=10)
         frame3.pack(side=TOP)
         button1 = ttk.Button(frame3, text="Solve", command=lambda: self.solve(maze, frame2))
@@ -104,11 +153,13 @@ class Window:
         return
 
     def solve(self, maze, frame):
+        # het callen van de functie uit het andere python bestand om een solution te vinden voor de opgegeven maze
         solution = tremaux(maze)
+
+        # het visueel maken van een pad van begin tot eind in het blauw in de eerder gemaakte maze
         self.draw_maze(maze, frame)
         self.draw(0, 0, 'blue', frame)
         for coord in solution:
-            print(coord)
             self.draw(coord[0], coord[1], 'blue', frame)
             frame.update()
         return
